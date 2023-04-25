9c7ff3dcbacc0258721b8475b474726b157d6c1c
Naveen belkale <bnaveenbhat@gmail.com>
2023-04-25 16:43:15 +0530

    Added feature to fetch weather data from openweathermap.org.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,9 @@
+import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+
+import '../services/networking.dart';
+
+const api_key = 'getyourownapikey';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -8,55 +12,30 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
-  void getLocation() async {
-    bool serviceEnabled;
-    LocationPermission permission;
+  void initState() {
+    super.initState();
+    getLocationData();
+  }
 
-    // Test if location services are enabled.
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the
-      // App to enable the location services.
-      return Future.error('Location services are disabled.');
-    }
+  void getLocationData() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    print('Position: ${location.latitude} ${location.longitude}');
 
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
-        return Future.error('Location permissions are denied');
-      }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      // Permissions are denied forever, handle appropriately.
-      return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions.');
-    }
-
-    // When we reach here, permissions are granted and we can
-    // continue accessing the position of the device.
-    Position position = await Geolocator.getCurrentPosition();
-    print(position);
+    var url = Uri.https('api.openweathermap.org', 'data/2.5/weather', {
+      'lat': location.latitude.toString(),
+      'lon': location.longitude.toString(),
+      'appid': api_key
+    });
+    NetworkHelper helper = NetworkHelper(url);
+    var weatherData = await helper.getData();
+    double temp = weatherData['main']['temp'];
+    int condition = weatherData['weather'][0]['id'];
+    String city = weatherData['name'];
+    print('Temp: $temp Condition: $condition City: $city');
   }
 
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            //Get the current location
-            getLocation();
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
---
+++ lib/services/networking.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class NetworkHelper {
+  final Uri url;
+  NetworkHelper(this.url);
+
+  Future getData() async {
+    var result = await http.get(url);
+    if (result.statusCode == 200) {
+      return jsonDecode(result.body);
+    } else {
+      print('HTTP Get failed with status code ${result.statusCode}');
+    }
+  }
+}
